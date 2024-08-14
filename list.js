846247ae19b61a6ed965b2b2e60b573da8372b24
wisdom014 <elijahakinniyi014@gmail.com>
2024-08-14 23:37:49 +0100

    fixed bug

--- list.js
+++ list.js
@@ -1,14 +1,16 @@
       function addItem(event) {
         event.preventDefault();
         const list = document.getElementById("list");
-        const itemText = list.value;
+        const itemText = list.value.charAt(0).toUpperCase() + list.value.slice(1); // Capitalize first letter
         const itemLists = document.getElementById("item-lists");
 
         const itemList = document.createElement("li");
         itemList.innerHTML = `
+        <div class="container">
           <input type="checkbox" onchange="toggleDone(this)" />
           <span>${itemText}</span>
           <button onclick="deleteItem(this)">Delete</button>
+        </div>
         `;
         itemLists.appendChild(itemList);
         list.value = "";
